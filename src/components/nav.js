47ab4cf86aba86f6b1f1a351193e6f9acb281cd7
Daniel KwanUk Yoo <kwanukyoo0722@gmail.com>
2019-02-18 18:02:24 -0800

    boiler plate for my resume (react)

---
+++ src/components/nav.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class Nav extends Component {
+    render() {
+        return(
+            <h1>this is going to nav bar</h1>
+        )
+    }
+}
