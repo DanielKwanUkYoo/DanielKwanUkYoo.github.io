22c5f98bbb071d3c3cf1f5c70216035a663b35bd
Daniel KwanUk Yoo <kwanukyoo0722@gmail.com>
2019-02-19 14:44:01 -0800

    about me tagline with icons

--- src/components/about.js
+++ src/components/about.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 import './styles/about.css'
 
+
 export default class About extends Component {
     render() {
-        return(
+        return(    
             <div className="about-container">
-
+                <div className="about-name">Daniel Yoo<br/>
+                    <div className="about-tagline">
+                    Junior Fullstack Developer<br/><br/><hr/><br/>
+                    <a href="#"><i className="fab fa-linkedin"></i></a>
+                    <a href="#"><i className="fab fa-github"></i></a>
+                    </div>
+                </div>
+                
             </div>
+
         )
     }
 }
