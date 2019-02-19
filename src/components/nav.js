aa282074b617bed26208daf9cf3ddc9afc927fb0
Daniel KwanUk Yoo <kwanukyoo0722@gmail.com>
2019-02-19 12:41:10 -0800

    navbar menus css

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,15 +2,43 @@ import React, {Component} from 'react';
 import './styles/nav.css'
 
 export default class Nav extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            initialScroll: 0,
+            navColor: "",
+            fontColor: ""
+        }
+    };
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll)
+    };
+
+    componentWillUnmount() {
+        window.removeEventListener('scorll', this.handleScroll)
+    };
+
+    handleScroll = () => {
+        const initialScroll = this.state.initialScroll;
+        const currentScroll = window.scrollY;
+        if (currentScroll > initialScroll) {
+            this.setState({ navColor: '#fff', fontColor: '#222' })
+        } else if (currentScroll < 1){
+            this.setState({ navColor: '#222', fontColor: '#fff'})
+        }
+    }
+    
     render() {
+
         return(
-            <nav className="nav-container">
+            <nav onScroll={this.handleScroll} className="nav-container" style={{backgroundColor: this.state.navColor}}>
                 <ul className="menu-lists">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About me</a></li>
-                    <li><a href="#">Skills</a></li>
-                    <li><a href="#">Projects</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a style={{color: this.state.fontColor}} href="#">Home</a></li>
+                    <li><a style={{color: this.state.fontColor}} href="#">About me</a></li>
+                    <li><a style={{color: this.state.fontColor}} href="#">Skills</a></li>
+                    <li><a style={{color: this.state.fontColor}} href="#">Projects</a></li>
+                    <li><a style={{color: this.state.fontColor}} href="#">Contact</a></li>
                 </ul>
             </nav>
         )
