f26ed4d4e589924ebe5d61a5152e84d9b31a4d0a
Daniel KwanUk Yoo <kwanukyoo0722@gmail.com>
2019-02-20 22:02:15 -0800

    added intro / skills

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ export default class About extends Component {
     render() {
         return(    
             <div className="about-container">
-                <div className="about-name">Daniel Yoo<br/>
+                <div className="about-name">DANIEL YOO<br/>
                     <div className="about-tagline">
                         Junior Fullstack Developer<br/><br/><hr/><br/>
                         <a href="#"><i className="fab fa-linkedin"></i></a>
--- src/components/skills.js
+++ src/components/skills.js
@@ -5,13 +5,31 @@ export default class Skills extends Component {
         return(
             <div>
                 <div className="intro-container">
-                    <h1>Background</h1>
-                    <h1>About</h1>
-                    <h1>Interests</h1>
+                    <h1 className="intro-title">Intro</h1>
+                    <p className="intro">
+                    <p>Hello, I'm Daniel.</p><br/>
+                    <p>Web Developer and natural leader who gains satisfaction by problem solving and code that empowers users. </p><br/>
+                    <p>I have an applied science background with teaching and management experience that gave me the foundation to excel in my passion of web development. </p><br/>
+                    <p>Love to stay curious, accept new challenges, seek for opportunities to grow and learn new technologies everyday.</p>
+                    </p>
                 </div>
                 <div className="skills-container">
-                    <h1>Front-End</h1>
-                    <h1>Back-End</h1>
+                    <div className="skills-title">
+                        <h1>Skills</h1> 
+                    </div>
+                    <div className="skills">
+                        <span className="front-end"><h1>Front-End</h1><br/>
+                        <p className="lists-of-skills">HTML5, CSS3, CSS Framework, Flexbox, Javascript, JS Framework( React, jQuery )</p>
+                        </span>
+                        <span className="back-end"><h1>Back-End</h1><br/>
+                        <p className="lists-of-skills">Node.js, Ruby, Ruby on Rails, PostgreSQL, MongoDB, RSpec( testing )</p>
+                        </span>
+                    </div>
+                </div>
+                <div className="projects-container">
+                    <div className="projects-title">
+                        <h1>Projects</h1>
+                    </div>
                 </div>
             </div>
         )
