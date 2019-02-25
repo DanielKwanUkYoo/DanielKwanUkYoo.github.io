ee6db8261fadd76bfde10ae1bc48b6e43b0b5f55
Daniel KwanUk Yoo <kwanukyoo0722@gmail.com>
2019-02-25 15:56:43 -0800

    basic outline for contacts/info is completed

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,22 +8,24 @@ export default class Contact extends Component {
                 <div className="contact-title">
                     <h2>Contact
                         <div className="contact-details">
-                            <p>Looking for opportunity to grow, </p>
-                            <p>learn and support!</p>
+                            <p>Looking for opportunity to grow, learn and support!</p>
                             <p>Hire me!</p>
                         </div>
                     </h2>
-                    
+                    <h4>Location
+                        <div className="contact-details">
+                            <p>Vancouver, BC</p>
+                        </div>
+                    </h4>
                     <h4>Email
                         <div className="contact-details">
                             <p>kwanukyoo@gmail.com</p>
                         </div>
                     </h4>
-                    <h4>media
-                        <div className="contact-details">
-                            <p></p>
-                        </div>
-                    </h4>
+                    <div className="contact-details">
+                        <a href="#"><i className="fab fa-linkedin"></i></a>
+                        <a href="#"><i className="fab fa-github"></i></a>
+                    </div>
                 </div>
 
                 {/* <div className="contact-tag-email-media">
