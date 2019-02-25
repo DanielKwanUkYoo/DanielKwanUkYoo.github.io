118aa8b1b4de12e23ecff7c67f3a5ee21bc77fc4
Daniel KwanUk Yoo <kwanukyoo0722@gmail.com>
2019-02-25 15:41:14 -0800

    outline for contact is completed

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,7 +5,32 @@ export default class Contact extends Component {
     render() {
         return(
             <div className="contact-container">
-                contact
+                <div className="contact-title">
+                    <h2>Contact
+                        <div className="contact-details">
+                            <p>Looking for opportunity to grow, </p>
+                            <p>learn and support!</p>
+                            <p>Hire me!</p>
+                        </div>
+                    </h2>
+                    
+                    <h4>Email
+                        <div className="contact-details">
+                            <p>kwanukyoo@gmail.com</p>
+                        </div>
+                    </h4>
+                    <h4>media
+                        <div className="contact-details">
+                            <p></p>
+                        </div>
+                    </h4>
+                </div>
+
+                {/* <div className="contact-tag-email-media">
+                    <p>Looking for opportunity to grow, learn and support!</p>
+                    <p>kwanukyoo@gmail.com</p>
+                    <p>media</p>
+                </div> */}
             </div>
         )
     }
